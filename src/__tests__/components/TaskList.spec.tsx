54d7594a63382fba5cfd571d0e6705eadf1fbe64
Tales Montenegro <montenegro.tales@gmail.com>
2021-09-23 20:51:05 -0300

    test(taskList): describe suit of taskList's tests

---
+++ src/__tests__/components/TaskList.spec.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+describe('<TaskList />', () => {
+    it('should be able to add a task', async () => {})
+    it('should not be able to add a task with a empty title', async () => {})
+    it('should be able to remove a task', async () => {})
+    it('should be able to check a task', async () => {})
+})
